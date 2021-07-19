52dd9d95da8a79c03efa7940b8d2f3d35ba7f57b
Ian Hopkins <ihopkins4249@gmail.com>
2021-07-19 19:13:55 +0000

    added code for writing keys to bootloader.c

--- tools/bl_build.py
+++ tools/bl_build.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python
 """
 Bootloader Build Tool
-
 This tool is responsible for building the bootloader from source and copying
 the build outputs into the host tools directory for programming.
 """
@@ -11,7 +9,7 @@ import pathlib
 import shutil
 import subprocess
 
-import secrets """i added these"""
+import secrets
 from Crypto.Hash import SHA256
 
 FILE_DIR = pathlib.Path(__file__).parent.absolute()
@@ -32,25 +30,33 @@ def copy_initial_firmware(binary_path):
 def make_bootloader():
     """
     Build the bootloader from source.
-
     Return:
         True if successful, False otherwise.
     """
-    
-    
-    signature = secrets.token_bytes(256) """i added this"""
-    f = open("secret_output.txt","w")
-    f.write(signature+"\n")
+
+    signature = secrets.token_bytes(256)
+    f = open("secret_output.txt", "w")
+    f.write(signature.decode() + "\n")
     h = SHA256.new()
     h.update(signature)
-
+    keys = [b"" for _ in range(200)]
     for i in range(200):
         keys[i] = secrets.token_bytes(16)
+    bc = open("filename", "r")
+    bootloader = []
+    x = 0
+    for l in bc.readlines():
+        if "Write Here" in l:
+            # Add key
+            index = l.find('""')
+            final = l[:index] + keys[x] + l[index:]
+            bootloader.append(final)
+            x+=1
+        else:
+            bootloader.append(l)
     for i in range(200):
-        f.write(keys[i].decode()+"\n")
+        f.write(keys[i].decode() + "\n")
     f.close()
-    
-    
 
     # Change into directory containing bootloader.
     bootloader = FILE_DIR / '..' / 'bootloader'
@@ -65,7 +71,9 @@ def make_bootloader():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Bootloader Build Tool')
-    parser.add_argument("--initial-firmware", help="Path to the the firmware binary.", default=None)
+    parser.add_argument("--initial-firmware",
+                        help="Path to the the firmware binary.",
+                        default=None)
     args = parser.parse_args()
     if args.initial_firmware is None:
         binary_path = FILE_DIR / '..' / 'firmware' / 'firmware' / 'gcc' / 'main.bin'
@@ -74,8 +82,8 @@ if __name__ == '__main__':
 
     if not os.path.isfile(binary_path):
         raise FileNotFoundError(
-            "ERROR: {} does not exist or is not a file. You may have to call \"make\" in the firmware directory.".format(
-                binary_path))
+            "ERROR: {} does not exist or is not a file. You may have to call \"make\" in the firmware directory."
+            .format(binary_path))
 
     copy_initial_firmware(binary_path)
     make_bootloader()
