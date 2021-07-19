ff6c96503d94c1583f8b5bb819836bec68064cf0
Geoffrey Qian <geoffrey.t.qian@gmail.com>
2021-07-19 12:37:21 -0700

    added fw protect mostly

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -2,8 +2,28 @@
 Firmware Bundle-and-Protect Tool
 
 """
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad
 import argparse
+import time
+import random
 import struct
+import hashlib
+
+
+def chunk(message):
+    cs = []
+    while len(message) > 0:
+        size = 128
+        if size > len(message):
+            size = len(message)
+        c = struct.pack("<i", size)[:-1]
+        for i in range(size):
+            c += chr(message[i]).encode()
+        message = message[size:]
+        cs.append(c)
+    return cs
 
 
 def protect_firmware(infile, outfile, version, message):
@@ -13,24 +33,52 @@ def protect_firmware(infile, outfile, version, message):
 
     # Append null-terminated message to end of firmware
     firmware_and_message = firmware + message.encode() + b'\00'
+    chunks = chunk(firmware_and_message)
+    lines = open("secret_build_output.txt", "rb").readlines()
+    signature = lines[0]
 
     # Pack version and size into two little-endian shorts
-    metadata = struct.pack('<HH', version, len(firmware))
+    metadata = struct.pack('<HHH', version, len(firmware), len(message))
 
-    # Append firmware and message to metadata
-    firmware_blob = metadata + firmware_and_message
+    keys = lines[1:]
+    random.seed(time.time())
+    data = [signature, metadata]
+    for c in chunks:
+        hash = hashlib.sha256(c).digest()
+        l = len(c)
+        kn = random.randint(0, 199)
+        k = keys[kn]
+        a = AES.new(k, AES.MODE_CBC, iv=get_random_bytes(16))
+        en = a.encrypt(pad(c, AES.block_size))
+        d = b""
+        d += struct.pack("<hh", kn, l)
+        d += en
+        d += hash
+        d += a.IV
+        data.append(d)
 
     # Write firmware blob to outfile
     with open(outfile, 'wb+') as outfile:
-        outfile.write(firmware_blob)
+        outfile.writelines(data)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Firmware Update Tool')
-    parser.add_argument("--infile", help="Path to the firmware image to protect.", required=True)
-    parser.add_argument("--outfile", help="Filename for the output firmware.", required=True)
-    parser.add_argument("--version", help="Version number of this firmware.", required=True)
-    parser.add_argument("--message", help="Release message for this firmware.", required=True)
+    parser.add_argument("--infile",
+                        help="Path to the firmware image to protect.",
+                        required=True)
+    parser.add_argument("--outfile",
+                        help="Filename for the output firmware.",
+                        required=True)
+    parser.add_argument("--version",
+                        help="Version number of this firmware.",
+                        required=True)
+    parser.add_argument("--message",
+                        help="Release message for this firmware.",
+                        required=True)
     args = parser.parse_args()
 
-    protect_firmware(infile=args.infile, outfile=args.outfile, version=int(args.version), message=args.message)
+    protect_firmware(infile=args.infile,
+                     outfile=args.outfile,
+                     version=int(args.version),
+                     message=args.message)
